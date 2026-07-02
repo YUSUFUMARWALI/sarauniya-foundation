bda6d2da2f5228f3d064645a70ecbbc7b2e0bb44
Yusuf Umar Wali <payveraofficial@gmail.com>
2026-07-03 03:55:58 +0530

    Add Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 ﻿import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -68,6 +69,19 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+      <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-49VL8KTHSZ"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-49VL8KTHSZ');
+  `}
+</Script>
         <div className="flex-1">{children}</div>
         <Footer /></body>
     </html>
